3f1c45db23528eaffa7b72c2fa89baa09fac1a8a
Arismário Neves <arismarioneves@users.noreply.github.com>
2023-10-02 22:27:02 -0300

    Analitycs

--- api/jazzicon.ts
+++ api/jazzicon.ts
@@ -1,27 +1,30 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
-import jazzicon from "./data/jazzicon";
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { inject } from '@vercel/analytics';
+import jazzicon from './data/jazzicon';
+
+inject();
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-	try {
-		const id = req.query.id as string;
-		
-		if (typeof id === "undefined") {
-			throw new Error("Param 'ID' is required");
-		}
-		
-		const seed = parseInt(id.slice(2, 10), 16);
-		
-		const identicon = jazzicon(100, seed);
-		
-		res.status(200);
-		res.setHeader("Content-Type", "image/svg+xml");
-		res.setHeader("Cache-Control", "max-age=0, s-maxage=86400");
-		res.send(identicon);
-		} catch (error) {
-		console.error(error);
-		
-		res.status(500).json({
-			error: error.message,
-		});
-	}
+    try {
+        const id = req.query.id as string;
+
+        if (typeof id === 'undefined') {
+            throw new Error("Param 'ID' is required");
+        }
+
+        const seed = parseInt(id.slice(2, 10), 16);
+
+        const identicon = jazzicon(100, seed);
+
+        res.status(200);
+        res.setHeader('Content-Type', 'image/svg+xml');
+        res.setHeader('Cache-Control', 'max-age=0, s-maxage=86400');
+        res.send(identicon);
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            error: error.message,
+        });
+    }
 }
